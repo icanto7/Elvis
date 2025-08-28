9555acec0d990ec1669915d36ddaa997b063386d
Ignacio <ignaciocantoisado@gmail.com>
2025-08-28 16:47:15 -0400

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Ignacio Canto on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
